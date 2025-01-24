336cd9d550b4e111512ae53a1ffe74199a761095
joshuaalegbe101 <joshuaalegbe101@gmail.com>
2025-01-24 02:51:54 -0600

    All request methods

--- server.js
+++ server.js
@@ -1,12 +1,30 @@
 const express = require("express");
+const products = require("./data/products");
+const reviews = require("./data/reviews");
 
 const app = express();
 const PORT = 3000;
+
+app.use((req, res, next) => {
+    console.log("Middleware is running");
+});
 
 app.listen(PORT, () => {
     console.log(`Server listening on port: ${PORT}.`);
 });
 
-app.get("/", (req, res) => {
+app.get("/api/products", (req, res) => {
     res.send("Hello Express");
 });
+
+app.get("/api/reviews", (req, res) => {
+    res.send("Hello Express");
+});
+
+app.put("./api/reviews", (req, res) => {
+    req.body;
+})
+
+app.delete("./api/reviews", (req, res) => {
+
+})
