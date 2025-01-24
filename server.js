864052d980e3911f49a4468eeb512e0cc5ce491e
joshuaalegbe101 <joshuaalegbe101@gmail.com>
2025-01-24 03:56:05 -0600

    All Crud

--- server.js
+++ server.js
@@ -10,22 +10,34 @@ app.use((req, res, next) => {
     console.log("Middleware is running");
 });
 
-app.listen(PORT, () => {
-    console.log(`Server listening on port: ${PORT}.`);
-});
-
+// Read
 app.get("/api/products", (req, res) => {
-    res.send("Hello Express");
+    res.json(products);
 });
 
 app.get("/api/reviews", (req, res) => {
-    res.send("Hello Express");
+    res.json(reviews)
 });
 
-app.put("./api/reviews", (req, res) => {
+app.get("/api/users", (req, res) => {
+    res.json(users)
+});
+
+// Update
+app.put("./api/products", (req, res) => {
+    req.body;
+});
+
+// Create
+app.post("./api/products", (req, res) => {
     req.body;
 })
 
+// Delete
 app.delete("./api/reviews", (req, res) => {
 
 })
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port: ${PORT}.`);
+});
